7f68342427059385d665885cf6b7454f78e95036
Walid Abdullah <walid@hufs.ac.kr>
2022-07-04 16:48:57 +0900

    subtraction added

--- complex.py
+++ complex.py
@@ -10,6 +10,14 @@ class Complex:
     
     def __str__(self):
         return str(self.re)+"+"+str(self.im)+"i"
+    
+    def subtract(self, c1): # (2+3i) - (1+2i) = 1+1i
+        c = Complex()
+        c.re = self.re - c1.re
+        c.im = self.im - c1.im
+        return c
 
 c1 = Complex(1,2)
 print(c1)
+c2 = Complex(2,3)
+print(c2.subtract(c1))
