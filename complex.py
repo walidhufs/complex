e7cbe959747e0df920d3eca99cb0ab40d8c6c12a
Walid Abdullah <walid@hufs.ac.kr>
2022-07-04 16:56:20 +0900

    multiplication added

--- complex.py
+++ complex.py
@@ -10,6 +10,14 @@ class Complex:
     
     def __str__(self):
         return str(self.re)+"+"+str(self.im)+"i"
+    
+    def multiply(self, c1): # (a+bi)(c+di)=ac+adi+bci-bd = (ac-bd) + (ad+bc)i
+        c = Complex()
+        c.re = self.re*c1.re - self.im*c1.im
+        c.im = self.re*c1.im + self.im*c1.re
+        return c
 
 c1 = Complex(1,2)
 print(c1)
+c2 = Complex(2,3)
+print(c1.multiply(c2))
